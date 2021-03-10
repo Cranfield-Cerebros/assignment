3f746f17dce0e4a1b467e26510eb9b77511cf046
adesbats <47774599+adesbats@users.noreply.github.com>
2021-03-10 13:46:17 +0100

    Update testInput.c

--- testInput.c
+++ testInput.c
@@ -4,11 +4,11 @@
 int main(){
 
     char path[] = "parameters.txt";
-    char mode = "r"
+    char mode[] = "r";
 
     FILE* fd = accessAFile(path, mode);
 
     modifyAField(fd, path, "alphaK1", 0.86);
-    
+
     return 0;
 }
